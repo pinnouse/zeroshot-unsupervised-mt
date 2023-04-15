3749ebb9034ba9926cce6134ba3b996974e7d7d4
Francis Dinh <normandy@biribiri.dev>
2023-04-15 17:38:35 -0400

    remove image and add missing imports

--- training.py
+++ training.py
@@ -12,6 +12,7 @@ PyTorch training for GAN:
 [PyTorch blog](https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html)
 """
 from models import Decoder, Transformer, Translator, Discriminator
+from data_loader import tokenizer
 from typing import List
 
 print(tokenizer.vocab_size)
