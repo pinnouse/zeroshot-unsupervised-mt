db52d5b0396f59869ed34d659b6ba7061be9797e
Nick <cupofpho@gmail.com>
2023-04-16 04:48:41 -0400

    Remove things that just run

--- data_loader.py
+++ data_loader.py
@@ -27,51 +27,51 @@ import random
 from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
-dataset = load_dataset("wikipedia", "20220301.simple")
-dataset_fr = load_dataset("wikipedia", "20220301.fr")
-dataset_ar = load_dataset('SaiedAlshahrani/Moroccan_Arabic_Wikipedia_20230101')
-dataset_lux = load_dataset("wikipedia", "20220301.frr")
-dataset_jp = load_dataset("AhmedSSabir/Japanese-wiki-dump-sentence-dataset")
+# dataset = load_dataset("wikipedia", "20220301.simple")
+# dataset_fr = load_dataset("wikipedia", "20220301.fr")
+# dataset_ar = load_dataset('SaiedAlshahrani/Moroccan_Arabic_Wikipedia_20230101')
+# dataset_lux = load_dataset("wikipedia", "20220301.frr")
+# dataset_jp = load_dataset("AhmedSSabir/Japanese-wiki-dump-sentence-dataset")
 
-training_percent = 0.8
-validation_percent = 0.1
-testing_percent = 0.1
+# training_percent = 0.8
+# validation_percent = 0.1
+# testing_percent = 0.1
 
 # english
-ds_en_len = len(dataset)
-test_index_en = ds_en_len * training_percent
-valid_index_en = ds_en_len * (training_percent + validation_percent)
+# ds_en_len = len(dataset)
+# test_index_en = ds_en_len * training_percent
+# valid_index_en = ds_en_len * (training_percent + validation_percent)
 
-# start to test index
-test_data_en = dataset['train'][: int(test_index_en)]
-# test index to validation index
-validation_data_en = dataset['train'][int(test_index_en):int(valid_index_en)]
-# validation index to end
-test_data_en = dataset['train'][int(valid_index_en):]
+# # start to test index
+# test_data_en = dataset['train'][: int(test_index_en)]
+# # test index to validation index
+# validation_data_en = dataset['train'][int(test_index_en):int(valid_index_en)]
+# # validation index to end
+# test_data_en = dataset['train'][int(valid_index_en):]
 
-# french
-ds_fr_len = len(dataset_fr)
-test_index_fr = ds_fr_len * training_percent
-valid_index_fr = ds_fr_len * (training_percent + validation_percent)
+# # french
+# ds_fr_len = len(dataset_fr)
+# test_index_fr = ds_fr_len * training_percent
+# valid_index_fr = ds_fr_len * (training_percent + validation_percent)
 
-# start to test index
-test_data_fr = dataset['train'][: int(test_index_fr)]
-# test index to validation index
-validation_data_fr = dataset['train'][int(test_index_fr):int(valid_index_fr)]
-# validation index to end
-test_data_fr = dataset['train'][int(valid_index_fr):]
+# # start to test index
+# test_data_fr = dataset['train'][: int(test_index_fr)]
+# # test index to validation index
+# validation_data_fr = dataset['train'][int(test_index_fr):int(valid_index_fr)]
+# # validation index to end
+# test_data_fr = dataset['train'][int(valid_index_fr):]
 
-# arabic
-ds_ar_len = len(dataset['train'])
-test_index_ar = ds_ar_len * training_percent
-valid_index_ar = ds_ar_len * (training_percent + validation_percent)
+# # arabic
+# ds_ar_len = len(dataset['train'])
+# test_index_ar = ds_ar_len * training_percent
+# valid_index_ar = ds_ar_len * (training_percent + validation_percent)
 
-# start to test index
-test_data_ar = dataset['train'][: int(test_index_ar)]
-# test index to validation index
-validation_data_ar = dataset['train'][int(test_index_ar):int(valid_index_ar)]
-# validation index to end
-test_data_ar = dataset['train'][int(valid_index_ar):]
+# # start to test index
+# test_data_ar = dataset['train'][: int(test_index_ar)]
+# # test index to validation index
+# validation_data_ar = dataset['train'][int(test_index_ar):int(valid_index_ar)]
+# # validation index to end
+# test_data_ar = dataset['train'][int(valid_index_ar):]
 
 def batch_loader(dataset, batch_size, shuffle=True):
   text = dataset['train']['text']
@@ -116,34 +116,34 @@ context_length = 64
 #glove = torchtext.vocab.GloVe(name="6B", dim=50)
 #ft = torchtext.vocab.FastText(language="simple")
 
-"""Tokenize/Embbed English data"""
+# """Tokenize/Embbed English data"""
 
-train_data_en = []
-for (i, t) in enumerate(text_long):
-  if i > 5:
-    break
-  tokenized = tokenizer(t, padding='max_length', max_length=64, return_tensors='pt').input_ids[0] #took 18 mins to run
-  if len(tokenized) <= 64:
-    sentences = []
-    for s in range(len(tokenized)):
-      sentences.append(tokenizer.decode(tokenized[1:s], skip_special_tokens=True))
-    #[bs x 64 x 512]
-    clips = text_model.encode(sentences)
-    train_data_en.append((t,clips,tokenized))
+# train_data_en = []
+# for (i, t) in enumerate(text_long):
+#   if i > 5:
+#     break
+#   tokenized = tokenizer(t, padding='max_length', max_length=64, return_tensors='pt').input_ids[0] #took 18 mins to run
+#   if len(tokenized) <= 64:
+#     sentences = []
+#     for s in range(len(tokenized)):
+#       sentences.append(tokenizer.decode(tokenized[1:s], skip_special_tokens=True))
+#     #[bs x 64 x 512]
+#     clips = text_model.encode(sentences)
+#     train_data_en.append((t,clips,tokenized))
 
-"""Tokenize/Embbed French data"""
+# """Tokenize/Embbed French data"""
 
-train_data_fr = []
+# train_data_fr = []
 
-for (i, t) in enumerate(text_fr):
-  if i > 5:
-    break
-  sentence = t[0]
-  if len(sentence.split(' ')) > 60:
-    continue
-  tokenized = tokenizer(sentence, padding='max_length', max_length=64, return_tensors='pt')['input_ids'][0]
-  # tokenized = tokenized.to(device)
-  train_data_fr.append((sentence, tokenized))
+# for (i, t) in enumerate(text_fr):
+#   if i > 5:
+#     break
+#   sentence = t[0]
+#   if len(sentence.split(' ')) > 60:
+#     continue
+#   tokenized = tokenizer(sentence, padding='max_length', max_length=64, return_tensors='pt')['input_ids'][0]
+#   # tokenized = tokenized.to(device)
+#   train_data_fr.append((sentence, tokenized))
 
 def data_loader(language):
   if language == 'en':
