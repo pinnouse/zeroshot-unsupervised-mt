4acff491424a60183c53c6f399b965f3ac79813e
Nick <cupofpho@gmail.com>
2023-04-16 21:49:02 -0400

    bruh

--- data_loader.py
+++ data_loader.py
@@ -232,7 +232,7 @@ def data_loader(language):
       page_sentences = clean_page.split('。' if language == 'jp' else '. ')
       tks = tokenizer(page_sentences, padding='max_length', max_length=64, return_tensors='pt', truncation=True)['input_ids']
       for i in range(len(page_sentences)):
-        sentence = sentences[i]
+        sentence = page_sentences[i]
         tokenized = tks[i]
 
         if len(tokenized) <= 64:
